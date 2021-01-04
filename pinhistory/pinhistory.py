13a6924d631ea9fb3c27b6cb2c0004ac5299d04d
CounterClops <19741838+CounterClops@users.noreply.github.com>
2021-01-04 16:48:26 +0800

    Further datetime tests

--- pinhistory/pinhistory.py
+++ pinhistory/pinhistory.py
@@ -115,7 +115,7 @@ class PinHistory(commands.Cog):
         embed_message = discord.Embed(description=message.content)
         embed_message.set_author(name=message.author.display_name, url="https://discord.com/users/{}".format(message.author.id), icon_url=message.author.avatar_url)
         #embed_message.set_thumbnail(message.author.avatar_url)
-        message_date = timezone("Australia/Perth").localize(message.created_at).strftime(r"%A, %X, %-d/%m/%Y")
+        message_date = timezone("Australia/Perth").localize(message.created_at.replace(tzinfo=pytz.UTC)).strftime(r"%A, %X, %-d/%m/%Y")
         embed_message.set_footer(text=message_date)
         return embed_message
 

ad73e3e5c488e42bc261356147da60212004f80e
CounterClops <19741838+CounterClops@users.noreply.github.com>
2021-01-04 16:21:43 +0800

    Things count from 0... don't judge me

--- pinhistory/pinhistory.py
+++ pinhistory/pinhistory.py
@@ -121,9 +121,7 @@ class PinHistory(commands.Cog):
         archive_channels = await self.config.guild(channel.guild).archive_channels()
         if (channel.id in monitored_channels) and (last_pin != None):
             # Check if this pin hasn't already been archived
-            last_pinned_message = (await channel.pins())[1]
-            print(await channel.pins())
-            print(last_pinned_message)
+            last_pinned_message = (await channel.pins())[0]
             async with self.config.guild(channel.guild).pin_history() as pin_history:
                 if last_pinned_message.id not in pin_history:
                     # Get list of attachments from message

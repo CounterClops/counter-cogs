13100ba6f92f2509bcb45dc8312a536bd1be5481
CounterClops <19741838+CounterClops@users.noreply.github.com>
2021-01-04 15:38:44 +0800

    Corrected handling of red bot config

--- pinhistory/pinhistory.py
+++ pinhistory/pinhistory.py
@@ -20,7 +20,7 @@ class PinHistory(commands.Cog):
             "manage_pins": True, # Whether the bot should manage pinned messages in the monitored channels (Remove old pins etc)
             "pin_history": [], # List of ID's for all pinned messages that have been archived
             "monitored_channels": [],  # Channels to monitor for changes in pins
-            "archive_channel": [], # Channels to archive the pinned messages to
+            "archive_channels": [], # Channels to archive the pinned messages to
         }
 
         self.config.register_guild(**default_guild)
@@ -33,16 +33,18 @@ class PinHistory(commands.Cog):
         Base postset group
         """
         # Should display current settings
-        await ctx.send("```{}```".format(str(self.config.guild(ctx.channel.guild))))
+        settings = await self.config.guild(ctx.channel.guild).get_raw()
+        await ctx.send("```{}```".format(str(settings)))
 
     @checks.admin()
     @pinhistory.group(name="pinlimit")
-    async def pinhistory_pinlimit(self, ctx, pin_limit : int):
+    async def pinhistory_pinlimit(self, ctx, new_pin_limit : int):
         """
         Set the number of pins allowed in a monitored channel
         """
-        self.config.guild(ctx.channel.guild).pin_limit = pin_limit
-        await ctx.send("Set pin limit to {}".format(pin_limit))
+        async with self.config.guild(ctx.channel.guild).pin_limit() as pin_limit:
+            pin_limit = new_pin_limit
+            await ctx.send("Set pin limit to {}".format(pin_limit))
 
     @checks.admin()
     @pinhistory.group(name="managepins")
@@ -50,9 +52,9 @@ class PinHistory(commands.Cog):
         """
         Toggle if pinned messages should be managed. EG, deleted once they go over pin limit
         """
-        self.config.guild(ctx.channel.guild).manage_pins = not self.config.guild(ctx.channel.guild).manage_pins
-        await ctx.send("Set pin management to {}".format(self.config.guild(ctx.channel.guild).manage_pins))
-
+        async with self.config.guild(ctx.channel.guild).manage_pins() as manage_pins:
+            manage_pins = not manage_pins
+            await ctx.send("Set pin management to {}".format(new_value))
 
     @checks.admin()
     @pinhistory.group(name="toggle")
@@ -62,12 +64,13 @@ class PinHistory(commands.Cog):
         """
         if channel == None:
             channel = ctx.channel
-        if channel.id not in self.config.guild(channel.guild).monitored_channels:
-            self.config.guild(channel.guild).monitored_channels.append(channel.id)
-            await ctx.send("Enabled monitoring for {}".format(channel.name))
-        else:
-            self.config.guild(channel.guild).monitored_channels.remove(channel.id)
-            await ctx.send("Disabled monitoring for {}".format(channel.name))
+        async with self.config.guild(channel.guild).monitored_channels() as monitored_channels:
+            if channel.id not in monitored_channels:
+                monitored_channels.append(channel.id)
+                await ctx.send("Enabled monitoring for {}".format(channel.name))
+            else:
+                monitored_channels.remove(channel.id)
+                await ctx.send("Disabled monitoring for {}".format(channel.name))
 
     # https://leovoel.github.io/embed-visualizer/
     # https://cog-creators.github.io/discord-embed-sandbox/
@@ -84,15 +87,17 @@ class PinHistory(commands.Cog):
     # Monitored Events
     @commands.Cog.listener('on_guild_channel_pins_update') # Executes the below command when a channels pinned messages changes
     async def on_pin_update(self, channel, last_pin):
-        if (channel.id in self.config.guild(channel.guild).monitored_channels) and (last_pin != None):
+        monitored_channels = await self.config.guild(channel.guild).monitored_channels()
+        archive_channels = await self.config.guild(channel.guild).archive_channels()
+        if (channel.id in monitored_channels) and (last_pin != None):
             # Check if this pin hasn't already been archived
-            if last_pin.id not in self.config.guild(channel.guild).pin_history:
-                # Get list of attachments from message
-                # Save list of attachment objects to a list of file like objects
-                # Send embed with provided attachments
-                embed_message = create_embed(last_pin)
-                for channel_id in await fetch_channel(self.config.guild(channel.guild).archive_channel):
-                    channel = await fetch_channel(channel_id)
-                    await channel.send(embed=embed_message)
-                self.config.guild(channel.guild).pin_history.append(last_pin.id)
-                pass
+            async for self.config.guild(channel.guild).pin_history() as pin_history:
+                if last_pin.id not in pin_history:
+                    # Get list of attachments from message
+                    # Save list of attachment objects to a list of file like objects
+                    # Send embed with provided attachments
+                    embed_message = create_embed(last_pin)
+                    for channel_id in archive_channels:
+                        channel = await fetch_channel(channel_id)
+                        await channel.send(embed=embed_message)
+                    pin_history.append(last_pin.id)
